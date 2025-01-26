13075d64df847d11244c3bbb9e6d739d74228cb0
Aaro <aaro.h4malainen@gmail.com>
2025-01-26 11:38:31 +0200

    New changes to start screen

--- Assets/Scripts/SwitchGameScene.cs
+++ Assets/Scripts/SwitchGameScene.cs
@@ -1,10 +1,33 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Control : MonoBehaviour
 {
-    public void NextScene()
+	public TMP_Text	replace;
+
+	public string[] strings;
+
+	public void NextScene()
     {
-        SceneManager.LoadScene("GameScene");
+        //SceneManager.LoadScene("GameScene");
+		StartCoroutine(StartCounting(strings));
+
     }
+
+	private IEnumerator StartCounting(string[] amogous)
+	{
+		replace.text = amogous[0];
+		yield return new WaitForSecondsRealtime(5);
+		replace.text = amogous[1];
+		yield return new WaitForSecondsRealtime(5);
+		replace.text = amogous[2];
+		yield return new WaitForSecondsRealtime(5);
+		replace.text = amogous[3];
+		yield return new WaitForSecondsRealtime(5);
+		replace.text = amogous[4];
+		yield return new WaitForSecondsRealtime(5);
+		SceneManager.LoadScene("GameScene");
+	}
 }
