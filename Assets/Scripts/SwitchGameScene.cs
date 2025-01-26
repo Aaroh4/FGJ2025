812d333b7fa509bbe2c332699e76b0c56face046
Aaro <aaro.h4malainen@gmail.com>
2025-01-26 14:40:33 +0200

    changes to start and end

--- Assets/Scripts/SwitchGameScene.cs
+++ Assets/Scripts/SwitchGameScene.cs
@@ -2,22 +2,41 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using Microsoft.Unity.VisualStudio.Editor;
 
 public class Control : MonoBehaviour
 {
 	public TMP_Text	replace;
+
+	public GameObject tree;
+
+	public GameObject hands;
+
+	public GameObject path;
+
+	public GameObject textImage;
+
+	public GameObject lastScreen;
 
 	public string[] strings;
 
 	public void NextScene()
     {
         //SceneManager.LoadScene("GameScene");
+		tree.SetActive(true);
 		StartCoroutine(StartCounting(strings));
 
     }
 
 	private IEnumerator StartCounting(string[] amogous)
 	{
+		yield return new WaitForSecondsRealtime(3);
+		path.SetActive(true);
+		yield return new WaitForSecondsRealtime(3);
+		hands.SetActive(true);
+
+		yield return new WaitForSecondsRealtime(3);
+		textImage.SetActive(true);
 		replace.text = amogous[0];
 		yield return new WaitForSecondsRealtime(5);
 		replace.text = amogous[1];
@@ -30,6 +49,9 @@ public class Control : MonoBehaviour
 		yield return new WaitForSecondsRealtime(5);
 		replace.text = amogous[5];
 		yield return new WaitForSecondsRealtime(5);
+		lastScreen.SetActive(true);
+		textImage.SetActive(false);
+		yield return new WaitForSecondsRealtime(4);
 		SceneManager.LoadScene("GameScene");
 	}
 }
